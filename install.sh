26d17f25c22a50db59c0f47145a19287d66396ed
ankul-shippable <ankul@shippable.com>
2016-04-04 14:31:40 +0530

    remove commands that are not needed #1

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ echo "======================= Installing gcc 5 ======================"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-5
-sudo update-alternatives
 sudo update-alternatives --remove-all gcc
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
 sudo update-alternatives --config gcc
